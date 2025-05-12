ea0b55b296670a9c271f36690972267b7d43bf8f
Ludka <mowjakchcesz@gmail.com>
2025-05-12 21:58:49 +0200

    commit msg

---
+++ my-app/app/components/header.tsx
@@ -0,0 +1,15 @@
+import "./header.scss";
+
+const Header = () => {
+  return (
+    <>
+      <svg>
+        <path d="" />
+      </svg>
+      <h1 id="test" className="heading">
+        <strong>LIZARD</strong>
+      </h1>
+    </>
+  );
+};
+export default Header;
